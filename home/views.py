aa378d130a2df172b7d0840178b9d030dcbe5b5d
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-04 19:52:06 +0000

    home template is added

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    """ This view returns index page from home """
+    return render(request, 'home/index.html')
