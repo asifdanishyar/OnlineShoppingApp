7a8366e22324dffa7c35c156928b4f32021a5c8d
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-16 16:55:00 +0000

    checkout is updated

--- checkout/views.py
+++ checkout/views.py
@@ -1,83 +1,19 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
-from products.models import Product
-
-# Create your views here.
+from .forms import OrderForm
 
 
-def view_bag(request):
-    """ A view that renders the bag contents page """
-
-    return render(request, 'bag/bag.html')
-
-
-def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
+def checkout(request):
     bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
 
-    if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id].keys():
-                bag[item_id] += quantity
-                messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
-            else:
-                bag[item_id] = quantity
-                messages.success(request, f'Added {product.name} to your bag')
-        else:
-            bag[item_id] = { quantity}
-            messages.success(request, f'Added {product.name} to your bag')
-    else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
-        else:
-            bag[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your bag')
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
 
-    request.session['bag'] = bag
-    return redirect(redirect_url)
-
-
-def adjust_bag(request, item_id):
-    """Adjust the quantity of the specified product to the specified amount"""
-
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    bag = request.session.get('bag', {})
-
-    if quantity > 0:
-        bag[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
-    else:
-        bag.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your bag')
-
-    request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
-
-
-def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
-
-    try:
-        product = get_object_or_404(Product, pk=item_id)
-        bag = request.session.get('bag', {})
-
-        if bag[item_id]:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
-
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
-
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+    return render(request, template, context)
