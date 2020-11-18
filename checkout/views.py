dc716a8292f07b45708372f39bae6c1685dc4f8a
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-18 17:23:50 +0000

    added order overlay... at the moment not working correct

--- checkout/views.py
+++ checkout/views.py
@@ -47,6 +47,7 @@ def checkout(request):
                                 order=order,
                                 product=product,
                                 quantity=quantity,
+                                product_size=size,
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
