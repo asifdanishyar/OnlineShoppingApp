ccee3f1dac27606ae1839ed60b9932fb1908c032
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-22 20:19:20 +0000

    profile app is updated. still having problem with the logging in

--- bag/views.py
+++ bag/views.py
@@ -5,12 +5,10 @@ from products.models import Product
 
 # Create your views here.
 
-
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
-
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -41,7 +39,7 @@ def add_to_bag(request, item_id):
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
-            
+
     request.session['bag'] = bag
     return redirect(redirect_url)
     
--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
 
-@login_required
+
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -15,12 +16,8 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request,
-                       ('update failed. please ensure'
-                        'the form is valid.'))
-    else:
-        form = UserProfileForm(instance=profile)
+
+    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -28,6 +25,23 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
