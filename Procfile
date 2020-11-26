27caf038afa7078d53ba2378186d171d09e361d6
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-26 17:26:38 +0000

    added hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn happy_shopping.wsgi:application
--- happy_shopping/settings.py
+++ happy_shopping/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'pz=233+^9n(v3dplfm#q=_=fymdf)ja^%d#)f#4cd%rx6*2kjg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['home-mate', 'localhost']
 
 
 # Application definition
@@ -117,18 +117,19 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'happy_shopping.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
