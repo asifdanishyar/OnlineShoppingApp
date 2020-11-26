389c4c5f300f8d4468622187ecc20987793221e0
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-26 18:58:52 +0000

    added static files

---
+++ custom_storages.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from storages.backends.s3boto3 import s3Boto3Storage
+
+class StaticStorage(s3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(s3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
