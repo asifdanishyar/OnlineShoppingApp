23dae9f68b2021d1b824d0c58e35a5272e163c40
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-26 16:52:33 +0000

    added heroku database

--- happy_shopping/settings.py
+++ happy_shopping/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +127,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
