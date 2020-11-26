7d11d0969d046e91171608a433c1f6f2e619d919
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-26 17:43:06 +0000

    app is successfully deployed to heroku

--- happy_shopping/settings.py
+++ happy_shopping/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pz=233+^9n(v3dplfm#q=_=fymdf)ja^%d#)f#4cd%rx6*2kjg'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['home-mate.herokuapp.com', 'localhost']
 
