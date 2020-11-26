6fcebf1d726b0209c6f9f7bee47fb489144bd9fb
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-26 19:20:28 +0000

    added cache control

--- happy_shopping/settings.py
+++ happy_shopping/settings.py
@@ -175,6 +175,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cach control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': '31 Dec 2099 20:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket conf
     AWS_STORAGE_BUCKET_NAME = 'home-mate'
     AWS_S3_REGION_NAME = 'eu-central-1'
