add98d3fefbe84572a574a193f47403df1876bea
asifdanishyar <asifdanishyar@yahoo.com>
2020-11-26 20:25:55 +0000

    temporarly object parameters deleted to see if error is coming from there

--- happy_shopping/settings.py
+++ happy_shopping/settings.py
@@ -177,10 +177,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Cach control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
+    
     # Bucket conf
     AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
     AWS_S3_REGION_NAME = 'us-east-1'
